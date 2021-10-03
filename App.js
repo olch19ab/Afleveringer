7d89964754b821209d6e23238c371efdd8f9eeaf
Oliver Dahl Christiansen <olch19ab@student.cbs.dk>
2021-10-03 19:46:08 +0200

    resterende filer

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
+//Her importere jeg de nødvendige dependencies for projektet.
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from "firebase";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {createStackNavigator} from "@react-navigation/stack"
+import { NavigationContainer } from '@react-navigation/native';
+import Add_Universities from "./components/Add_Universities";
+import UniList from "./components/UniList";
+import Icon from 'react-native-vector-icons/Ionicons';
+import { getDatabase } from "firebase/database";
+
+
+// Dette er config-oplysningerne, som jeg har fra min firebase database
+const firebaseConfig = {
+  apiKey: "AIzaSyDAP_XHPJdwjMhJW8we7UJsjjDhSPkFIds",
+  authDomain: "exercise-5-v2.firebaseapp.com",
+  databaseURL: "https://exercise-5-v2-default-rtdb.firebaseio.com",
+  projectId: "exercise-5-v2",
+  storageBucket: "exercise-5-v2.appspot.com",
+  messagingSenderId: "378224061040",
+  appId: "1:378224061040:web:2071f934a9ca3606a9e2b5"
+};
+
+// Konstanter for stac- og tabnavigator
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+// Her initialisere jeg applikationen
 
 export default function App() {
+  
+  function StackNavigation() {
+
+    return (
+
+      // Stack.navigatoren med de to tilhørended screens, for de to sides i projektet.
+
+      <Stack.Navigator>
+        
+  
+        <Stack.Screen name="UniList" component={UniList} />
+       
+        
+        <Stack.Screen name="Add_Universities" component={Add_Universities} />
+
+      
+      </Stack.Navigator>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // Her retunere jeg de to tab screens, som er menu bare ikonerne i applikationen.
+<NavigationContainer>
+        <Tab.Navigator>
+           
+           <Tab.Screen name={'University List'} component={UniList} options={{tabBarIcon: () => ( <Icon name="home" size={32}/>)}}/>
+           <Tab.Screen name={'Add'} component={Add_Universities} options={{tabBarIcon: () => ( <Icon name="book" size={20} />)}}/>
+
+        </Tab.Navigator>
+      </NavigationContainer>
+   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
